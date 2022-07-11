8f06f46f089051f531e8421e8a315d42132abfe7
ZTStew <ZTStew>
2022-07-11 15:17:17 -0400

    Source Commit

---
+++ happy_birthday_tts.py
@@ -0,0 +1,44 @@
+"""
+
+Author: Zachary Stewart
+Date: 07/07/2022
+
+Description: Program takes in a user's name and age and generates a TTS response saying happy birthday.
+
+"""
+
+import pyttsx3
+
+# Initializes instance of pyttsx3
+tts = pyttsx3.init()
+
+# Tells user speakers would be needed
+print("Make sure you have your speakers turned on!")
+
+# Prompts user for their name
+name = input("Enter Your Name: ")
+age = input("Enter Your Age: ")
+
+# Song being read out
+lyrics = """
+Happy Birthday to You,
+You are [age],
+Happy Birthday Dear [name]
+Happy Birthday to You.
+"""
+
+# Inserts given name into song lyrics
+lyrics = lyrics.replace("[name]", name)
+lyrics = lyrics.replace("[age]", age)
+
+# volume = tts.getProperty('volume')
+# print (volume)
+
+# Changes voice being used
+voices = tts.getProperty('voices')
+# Keeping voice as default (voices[0].id)
+tts.setProperty('voice', voices[0].id)
+
+# Tells program to say given string
+tts.say(lyrics)
+tts.runAndWait()
